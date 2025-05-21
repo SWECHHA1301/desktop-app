1cb6594a1345e5b728e8778999bae4e9c5e096a0
SWECHHA1301 <swechhasharma231@gmail.com>
2025-05-21 21:37:45 +0530

    Update App.js

--- DA/src/App.js
+++ DA/src/App.js
@@ -3,7 +3,6 @@ import Navbar from './Common/Navbar';
 import Sidebar from './Common/Sidebar';
 // import Dashboard from './Product-Info/Dashboard/Dashboard';
 import TableDashboard from './Table Management/TableDashboard';
-import PurchaseReceipt from './Common/PurchaseReciept';
 import ProductCard from './Product/ProductCard';
 // import TotalItem from './Product-Info/Dashboard/Components/TotalBlock/TotalItem';
 // import SellingItem from './Product-Info/Dashboard/Components/SellingItem/SellingItem';
