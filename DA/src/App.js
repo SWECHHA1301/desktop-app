2a3f1e78cbf7845841813fa02f8ef890ca2ca770
Aman Singh <amansingh6131@gmail.com>
2025-05-23 22:12:59 +0530

    update

--- DA/src/App.js
+++ DA/src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import Navbar from './Common/Navbar';
-import Sidebar from './Common/Sidebar';
-import ProductCard from './Product/ProductCard';
-
+import React from "react";
+import Navbar from "./Common/Navbar";
+import Sidebar from "./Common/Sidebar";
+import ProductCard from "./Product/ProductCard";
+import TableDashboard from "./Table Management/TableDashboard";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Sidebar />
-      <ProductCard />
-      </div>
-
+      {/* <ProductCard /> */}
+      <TableDashboard />
+    </div>
   );
 }
 
 export default App;
-
