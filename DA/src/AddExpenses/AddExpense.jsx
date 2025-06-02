ba261a712afa6f0f47e86d3aad44a80d0cae70cb
Harshita <hc8269851@gmail.com>
2025-06-02 15:43:31 +0530

    updated

--- DA/src/AddExpenses/AddExpense.jsx
+++ DA/src/AddExpenses/AddExpense.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import SaveButton from '../Common/SaveButton';
+import { ChevronLeft, ChevronRight ,ChevronDown} from "lucide-react";
+import SaveButton from '../Common/SaveButton'
+import OtherCharges from './components/OtherCharges';
+import PaymentPaid from './components/PaymentPaid';
+
 
 export default function AddExpense() {
+  
   return (
     <>
     <div style={{width:'100%',maxWidth:'1318px',height:'885px',backgroundColor:'#D4DAED',padding:'16px 24px',
@@ -25,15 +29,15 @@ export default function AddExpense() {
          <SaveButton/>
          </div>
 
-         <div style={{maxWidth:'860px',height:'742px',backgroundColor:'#fff',
-            borderRadius:'10px',boxShadow:'0 4px 4px 0 #00000040',
+         <div style={{maxWidth:'860px',backgroundColor:'#fff',
+            borderRadius:'10px',boxShadow:'0 4px 4px 0 #00000040', paddingBottom:'16px'
          }}>
-           
-           <div style={{}}>
+           {/* heading */}
+           <div>
            <p style={{fontSize:'24px',fontWeight:'700',lineHeight:'100%',padding:'21px 32px',
             borderBottom:'1px solid #D4DAED'}}>Add Sale</p>
            </div>
-
+           {/* invoice no. */}
            <div style={{display:'flex',justifyContent:'space-between',alignItems:'center',
              padding:'8px 16px', borderBottom:'1px solid #D4DAED'}}>
 
@@ -51,8 +55,24 @@ export default function AddExpense() {
             </div>
 
            </div>
+           
+           <OtherCharges/>
+           <PaymentPaid/>
+
+
+         
+
          </div>
+   
+   
+   
+   
     </div>
+
+
+
+
     </>
+
   )
 }
---
+++ DA/src/AddExpenses/components/OtherCharges.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { ChevronLeft, ChevronRight ,ChevronDown} from "lucide-react";
+import { useState } from 'react';
+
+export default function OtherCharges() {
+      const [isOpen, setIsOpen] = useState(true);
+  return (
+   <>
+   <div style={{padding:'16px 24px', borderBottom:'1px solid #D4DAED'}}>
+
+            <div onClick={() => setIsOpen(!isOpen)} style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+            <p style={{fontSize:'16px',fontWeight:'500',lineHeight:'100%',}}>Other Charges</p>
+
+              <div style={{cursor:'pointer'}}>
+          {isOpen ? (
+            <ChevronDown style={{ color: "#939191" }} size={20}/>
+          ) : (
+            <ChevronRight style={{ color: "#939191" }} size={20}/>
+          )}
+        </div>
+            </div>
+               
+               {isOpen && (
+               <div style={{marginTop:'24px'}}>
+
+               <div style={{display:'flex',flexDirection:'column',gap:'16px' }} >
+ 
+             <div style={{display:'flex',flexDirection:'column', gap:'8px',fontSize:'14px',fontWeight:'400',lineHeight:'100%'}}>
+            <label style={{color:'#797979'}}>Expense Category</label>
+            <input style={{
+              maxWidth:'390px',
+              height:'27px',
+              backgroundColor:'#E3E6EE',
+              border:'1px solid #ccc',
+              borderRadius:'5px',
+              
+            }} type='text' placeholder='Enter Expense Category' defaultValue={'Expense Category'}/>
+          </div>
+
+            <div style={{display:'flex',flexDirection:'column', gap:'8px',fontSize:'14px',fontWeight:'400',lineHeight:'100%'}}>
+            <label style={{color:'#797979'}}>Amount</label>
+            <input style={{
+              maxWidth:'390px',
+              height:'27px',
+              backgroundColor:'#E3E6EE',
+              border:'1px solid #ccc',
+              borderRadius:'5px',
+              
+            }} type='number' placeholder='Enter Amount' defaultValue={180000.00}/>
+            
+            {/* {₹}  */}
+          </div>
+
+            <div style={{display:'flex',flexDirection:'column', gap:'8px',fontSize:'14px',fontWeight:'400',lineHeight:'100%'}}>
+            <label style={{color:'#797979'}}>Expense Description</label>
+            <textarea style={{
+              maxWidth:'390px',
+              height:'93px',
+              resize:'none',
+              backgroundColor:'#E3E6EE',
+              border:'1px solid #ccc',
+              borderRadius:'5px',
+              paddingTop:'6px',
+            }} type='text' placeholder='Enter Expense Description'/>
+          </div>
+
+          </div>
+         
+
+           <div style={{display:'flex',alignItems:'center', gap:'16px',justifyContent:'center',
+            maxWidth:'141px',height:'31px',border:'1.5px solid #3658BF',borderRadius:'5px',marginTop:'24px'}}>
+            <p style={{fontSize:'12px',fontWeight:'700',lineHeight:'100%',color:'#3658BF'}}>Attachment</p>
+           </div>
+           
+            </div>)}
+
+          </div>
+   </>
+  )
+}
